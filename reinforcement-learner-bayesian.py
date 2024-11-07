6452ef66e82c30cb4b03a9757bf2eacf7ead7b71
thornwell-labs <thornwell.labs@gmail.com>
2024-11-07 13:32:33 -0800

    Uploaded version 3 of reinforcement learning tool
    Updated kernel function based on kernel test results. Added measure of GPR model fitness and updated pair-wise plotting of GPR to be useful. Added data dependency on number of VELMA parameters.

--- reinforcement-learner-bayesian.py
+++ reinforcement-learner-bayesian.py
@@ -5,38 +5,44 @@ import subprocess
 import os
 import shutil
 from skopt import gp_minimize
-from skopt.space import Real
 from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
 
 
 # Define the required run parameters and directories
-start_year = 1969
-start_learning_year = 1972
-end_year = 2005
+start_year = 1987
+start_learning_year = 1990
+end_year = 2021
 allocated_memory = "-Xmx20G"
-jar_path = "C:/Users/thorn/OneDrive/Desktop/JVelma_CW3-495_v02_GBox.jar"
-working_directory = 'C:/Users/thorn/Documents/Automated_Calibration/WS10'
-xml_name = 'OR_BR_ws10_14Oct24'
-xml_path = f'{working_directory}/XMLs/{xml_name}.xml'
+jar_path = "C:/Users/thorn/OneDrive/Desktop/JVelma_dev-test_v002.jar"
+working_directory = 'C:/Users/thorn/Documents/VELMA_Watersheds/Hoko/Hoko_Working'
+xml_name = 'WA_Hoko30m_1Nov2024'
+xml_path = f'{working_directory}/XML/{xml_name}.xml'
 results_folder_root = f'{working_directory}/Results'
 q_table_output = f'{results_folder_root}/q-table.csv'
 running_average_output = f'{results_folder_root}/running-averages.csv'
 figure_path = f'{results_folder_root}/Figures'
-velma_parallel = False
+velma_parallel = True
 # default_results file contains starting NSE values for comparison
-default_results = f'{results_folder_root}/MULTI_WS10_DefaultRun/Results_2932/AnnualHydrologyResults.csv'
-
+default_results = f'{results_folder_root}/WA_Hoko_24Oct2024_Results/AnnualHydrologyResults.csv'
 # Required run parameters if velma_parallel is True
-outlet_id = '2932'
+outlet_id = '100002'
 max_processes = "6"
 
 # Define starting and allowable VELMA parameter ranges
 velma_parameters = {
-    '/calibration/VelmaInputs.properties/be': {'name': 'be','value': 1.0, 'min': 0.7, 'max': 1.2},
-    '/soil/Soil1_sandy_loam/surfaceKs': {'name': 'Soil1_surfaceKs','value': 600, 'min': 500, 'max': 1200}
-    # '/': {'value': 1.0, 'min': 0.5, 'max': 1.5},
-    # Can add/remove parameters
+    '/calibration/VelmaCalibration.properties/setGroundwaterStorageFraction': {'name': 'GroundwaterStorageFraction','value': 0, 'min': 0.0, 'max': 0.2},
+    '/calibration/VelmaCalibration.properties/f_ksv': {'name': 'VerticalKs', 'value': 0.0013, 'min': 0.001, 'max': 0.002},
+    '/calibration/VelmaCalibration.properties/f_ksl': {'name': 'LateralKs', 'value': 0.00155, 'min': 0.001, 'max': 0.002},
+    '/soil/Lowland_Loam/soilColumnDepth': {'name': 'MediumSoilDepth','value': 1080, 'min': 1080, 'max': 1300},
+    '/soil/LowlandLoam_Shallow/soilColumnDepth': {'name': 'ShallowSoilDepth','value': 470, 'min': 470, 'max': 1080},
+    '/soil/LowlandLoam_Deep/soilColumnDepth': {'name': 'DeepSoilDepth','value': 1530, 'min': 1300, 'max': 3000},
+    '/soil/Lowland_Loam/surfaceKs': {'name': 'MediumKs','value': 1200, 'min': 400, 'max': 2000},
+    '/soil/LowlandLoam_Shallow/surfaceKs': {'name': 'ShallowKs','value': 1200, 'min': 400, 'max': 2000},
+    '/soil/LowlandLoam_Deep/surfaceKs': {'name': 'DeepKs','value': 1200, 'min': 400, 'max': 2000}
+    # Can add/remove parameters, but doing so necessitates manual changes to q-table and running-average table
 }
 
 # List of parameter values for easy dataframe writing
@@ -71,63 +77,84 @@ else:
         subprocess.run(command_str, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # Initiate NSE using AnnualHydrologyResults
-if not os.path.exists(f'{results_folder_root}/{xml_name}_spinup'):
-    os.rename(f'{results_folder_root}/{xml_name}', f'{results_folder_root}/{xml_name}_spinup')
 if velma_parallel:
-    simulated_results = pd.read_csv(f'{results_folder_root}/{xml_name}_spinup/'
+    if not os.path.exists(f'{results_folder_root}/MULTI_{xml_name}_spinup'):
+        os.rename(f'{results_folder_root}/MULTI_{xml_name}', f'{results_folder_root}/MULTI_{xml_name}_spinup')
+else:
+    if not os.path.exists(f'{results_folder_root}/{xml_name}_spinup'):
+        os.rename(f'{results_folder_root}/{xml_name}', f'{results_folder_root}/{xml_name}_spinup')
+if velma_parallel:
+    simulated_results = pd.read_csv(f'{results_folder_root}/MULTI_{xml_name}_spinup/'
                                     f'/Results_{outlet_id}/AnnualHydrologyResults.csv')
 else:
     simulated_results = pd.read_csv(f'{results_folder_root}/{xml_name}_spinup'
                                     f'/AnnualHydrologyResults.csv')
 nse = simulated_results.loc[simulated_results['YEAR'] == end_spinup_year, 'Runoff_Nash-Sutcliffe_Coefficient']
 nse = nse.values[0]
+precip = simulated_results.loc[simulated_results['YEAR'] == end_spinup_year, 'Total_Rain+Melt(mm)']
+precip = precip.values[0]
 
 print(f'End spin-up year had NSE = {nse}.')
 
 # Calculate the reward by comparing the new performance to the default performance
-default_df = pd.read_csv(default_results, usecols=['YEAR', 'Runoff_Nash-Sutcliffe_Coefficient'])
+default_df = pd.read_csv(default_results, usecols=['YEAR', 'Runoff_Nash-Sutcliffe_Coefficient', ])
 
 def evaluate_model(eval_year, eval_nse, defaults=default_df):
     default_nse = default_df.loc[default_df['YEAR'] == eval_year, 'Runoff_Nash-Sutcliffe_Coefficient'].values[0]
-    reward = 100*(eval_nse - default_nse)
+    reward = eval_nse - default_nse
     return reward
-    
+
 reward = evaluate_model(end_spinup_year, nse)
 
 # Initialize Q-table
 # Check whether the file exists so that old data isn't overwritten
 if not os.path.isfile(q_table_output):
-    q_table = pd.DataFrame(columns=list(velma_parameters.keys())+['Reward', 'NSE', 'Year'])
+    q_table = pd.DataFrame(columns=list(velma_parameters.keys())+['Reward', 'NSE', 'Year', 'Precip'])
     q_table.to_csv(q_table_output, mode='w', index=False)
 else:
     q_table = pd.read_csv(q_table_output)
-q_table.loc[len(q_table)] = parameter_values + [reward, nse, end_spinup_year]
+q_table.loc[len(q_table)] = parameter_values + [reward, nse, end_spinup_year, precip]
 q_table.loc[[len(q_table)-1]].to_csv(q_table_output, mode='a', header=False, index=False)
 print('Q-table initialized.')
 
 # Check if running_averages.csv exists; if not, need to initialize it
 # Initialize table of parameters to record unique values and average NSE
-if os.path.exists(running_average_output):
+if os.path.isfile(running_average_output):
     running_average = pd.read_csv(running_average_output)
 else:
     running_average = pd.DataFrame(columns=list(velma_parameters.keys())+['Average_Reward'])
     running_average.loc[0] = parameter_values + [reward]
+    running_average.to_csv(running_average_output, index=False)
 
 print('Q-table and average reward table initialized.')
 
 # Scale the data for use in GPR and Bayesian optimization
+# Check whether the current parameter bounds or the min/max in the data should be used for scaling
 scaler = MinMaxScaler()
 param_bounds = ([[velma_parameters[param]['min'], velma_parameters[param]['max']] for param in velma_parameters])
-param_bounds = np.array(param_bounds).T
-scaled_bounds = scaler.fit_transform(param_bounds)
-scaled_bounds = scaled_bounds.T
-param_space = [(bound[0], bound[1]) for bound in scaled_bounds]
+X = running_average.iloc[:, :-1].values
+X_min = X.min(axis=0).tolist()
+X_max = X.max(axis=0).tolist()
+param_space = []
+for i, min in enumerate(X_min):
+    if min < param_bounds[i][0]:
+        data_min = min
+    else:
+        data_min = param_bounds[i][0]
+    if X_max[i] > param_bounds[i][1]:
+        data_max = X_max[i]
+    else:
+        data_max = param_bounds[i][1]
+    param_space.append([data_min, data_max])
+param_space = np.array(param_space).T
+param_space = scaler.fit_transform(param_space)
+param_space = param_space.T
+param_space = [(bound[0], bound[1]) for bound in param_space]
+X_scaled = scaler.transform(X)
 
 # Gaussian Process Regression (surrogate model)
-X = running_average.iloc[:, :-1].values
-X_scaled = scaler.transform(X)
 Y = running_average['Average_Reward'].values
-gp_model = GaussianProcessRegressor(n_restarts_optimizer=20)
+gp_model = GaussianProcessRegressor(n_restarts_optimizer=20, kernel=RBF()+WhiteKernel(noise_level_bounds=(1e-3, 1.0)))
 gp_model = gp_model.fit(X_scaled, Y)
 
 def objective(parameters):
@@ -142,10 +169,10 @@ for year in range(start_learning_year, end_year+1):
     start_data = f'{results_folder_root}/Results_{str(year - 1)}'
     end_data = f'{results_folder_root}/Results_{str(year)}'
     
-    # If there are less than 8 points in the q_table, force random exploration of the parameter space
-    if len(q_table) < 8:
+    # If there are less than (3 * #parameters) unique data points, force random exploration of the parameter space
+    if len(running_average) < 3*len(velma_parameters):
         print(f"Q-table is too sparse. Forcing random exploration of parameter space.")    
-        parameter_values = [round(np.random.uniform(velma_parameters[param]['min'], velma_parameters[param]['max']), 2) 
+        parameter_values = [round(np.random.uniform(velma_parameters[param]['min'], velma_parameters[param]['max']), 5) 
                             for param in velma_parameters]
         for idx, param in enumerate(velma_parameters.keys()):
             velma_parameters[param]['value'] = parameter_values[idx]
@@ -176,16 +203,17 @@ for year in range(start_learning_year, end_year+1):
              *parameter_modifiers]
         command_str = ' '.join(command)
         subprocess.run(command_str, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        
 
     # Find NSE using AnnualHydrologyResults
     if velma_parallel:
-        simulated_results = pd.read_csv(f'{results_folder_root}/{xml_name}/'
+        simulated_results = pd.read_csv(f'{results_folder_root}/MULTI_{xml_name}/'
                                         f'Results_{outlet_id}/AnnualHydrologyResults.csv')
     else:
         simulated_results = pd.read_csv(f'{results_folder_root}/{xml_name}/AnnualHydrologyResults.csv')
     nse = simulated_results.loc[simulated_results['YEAR'] == year, 'Runoff_Nash-Sutcliffe_Coefficient']
     nse = nse.values[0]
+    precip = simulated_results.loc[simulated_results['YEAR'] == year, 'Total_Rain+Melt(mm)']
+    precip = precip.values[0]
     
     print(f"{year} had NSE of {round(nse, 2)}.")
     
@@ -194,7 +222,7 @@ for year in range(start_learning_year, end_year+1):
     print(f"Reward for {year} was {round(reward, 3)}.")
 
     # Update the Q-table
-    q_table.loc[len(q_table)] = parameter_values + [reward, nse, year]
+    q_table.loc[len(q_table)] = parameter_values + [reward, nse, year, precip]
     q_table.loc[[len(q_table)-1]].to_csv(q_table_output, mode='a', header=False, index=False)
     
     # Update the running average table
@@ -212,37 +240,44 @@ for year in range(start_learning_year, end_year+1):
             row_index = existing_row_mask.idxmax()  # Get the index of the first matching row
             running_average.at[row_index, 'Average_Reward'] = mean_reward
         else:
-            # Create a new row for the running average DataFrame
+            # Create a new row for the running average
             new_row = parameter_values + [mean_reward]
             running_average.loc[len(running_average)] = new_row
-            
+
     # Save the running_averages to a .csv
     running_average.to_csv(running_average_output, index=False)
-    
-    # Update surrogate model (Gaussian Process Regression)
+
+    # Update GPR model with new data
     X = running_average.iloc[:, :-1].values
     X_scaled = scaler.transform(X)
     Y = running_average['Average_Reward'].values
     gp_model.fit(X_scaled, Y)
     
+    # Y_pred for R^2 score
+    Y_pred, sigma = gp_model.predict(X_scaled, return_std=True)
+    r2 = r2_score(Y, Y_pred)
+    print(f'R^2 score for the GPR was {r2:.4f}')
+
+
     # Plot the GPR
-    X_pred = np.column_stack([np.linspace(velma_parameters[param]['min'], velma_parameters[param]['max'], 100)
-        for param in velma_parameters])
-    X_pred_scaled = scaler.transform(X_pred)
-    Y_pred, sigma = gp_model.predict(X_pred_scaled, return_std=True)
-    
-    for i, param in enumerate(velma_parameters.keys()):
+    for i, col in enumerate(X.T):
         plt.figure(figsize=(10, 6))
-        plt.plot(X[:, i], Y, 'r.', markersize=10, label='Observed Data')
-        plt.plot(X_pred[:, i], Y_pred, 'b-', label='GPR Prediction')
-        plt.fill_between(X_pred[:, i].flatten(), Y_pred - 1.96 * sigma, Y_pred + 1.96 * sigma,
+        plt.plot(col, Y, 'r.', markersize=10, label='Observed Data')
+        
+        # Sort predictions before plotting
+        sorted_indices = np.argsort(col)  # Sort by the values in the current column of X
+        X_sorted = col[sorted_indices]  # Sort the current column
+        Y_pred_sorted = Y_pred[sorted_indices]
+        
+        plt.plot(X_sorted, Y_pred_sorted, 'b-', label='GPR Predictions')
+        plt.fill_between(X_sorted, Y_pred_sorted - 1.96 * sigma, Y_pred_sorted + 1.96 * sigma,
                         alpha=0.2, color='blue', label='95% Confidence Interval')
-        plt.title(f'Gaussian Process Regression Update for {velma_parameters[param]["name"]}')
-        plt.xlabel(param)
+        plt.title(f'Gaussian Process Regression Update for {velma_parameters[i]}')
+        plt.xlabel(f'{velma_parameters[i]}')
         plt.ylabel('Rewards')
         plt.legend()
         plt.grid()
-        plt.savefig(f'{figure_path}/gpr_{year}_{velma_parameters[param]["name"]}')
+        plt.savefig(f'{figure_path}/test/gpr_{velma_parameters[i]}')
         plt.close()
     
     # Run Bayesian optimization to estimate the new parameter set
@@ -257,9 +292,7 @@ for year in range(start_learning_year, end_year+1):
     
     best_scaled_params = result.x
     best_parameters = scaler.inverse_transform([best_scaled_params])[0]
-    best_parameters = [round(param, 2) for param in best_parameters]
-    # best_parameters = [np.clip(best_parameters[i], velma_parameters[param]['min'], velma_parameters[param]['max'])
-                    #    for i, param in enumerate(velma_parameters.keys())]
+    best_parameters = [round(param, 5) for param in best_parameters]
 
     # Update VELMA parameters for the next run
     for idx, param in enumerate(velma_parameters.keys()):
@@ -267,7 +300,7 @@ for year in range(start_learning_year, end_year+1):
     parameter_values = [velma_parameters[param]["value"] for param in velma_parameters]
     parameter_modifiers = [f'--kv="{param},{velma_parameters[param]["value"]}"' for param in velma_parameters]
 
-    # Print adjusted parameters for the current year
+    # Print adjusted parameters for the next year
     print(f"Updated parameters for year {year+1}:")
     for param in velma_parameters.keys():
         print(f"{velma_parameters[param]['name']}: {velma_parameters[param]['value']}")       
@@ -275,6 +308,12 @@ for year in range(start_learning_year, end_year+1):
     # Delete unnecessary data to save space
     if year > start_learning_year+2:
         shutil.rmtree(f'{results_folder_root}/Results_{str(year-2)}')
-    shutil.rmtree(f'{results_folder_root}/{xml_name}')
+    if velma_parallel:
+        shutil.rmtree(f'{results_folder_root}/MULTI_{xml_name}')
+    else:
+        shutil.rmtree(f'{results_folder_root}/{xml_name}')
         
 print("Calibration process completed!")
+print("Best predicted parameters:")
+for param in velma_parameters.keys():
+    print(f"{velma_parameters[param]['name']}: {velma_parameters[param]['value']}")    
